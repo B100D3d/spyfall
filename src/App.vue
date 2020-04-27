57b70e01d12d1d32c9f45e2cc037d2e7d64065d1
devourer <alexblooded@gmail.com>
2020-04-27 10:44:11 +0300

    Create project and ThemeButton

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+    div(id="app")
+        ThemeButton
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ThemeButton from "@/components/ThemeButton"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+      ThemeButton
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+    *
+        outline: none
+        box-sizing: border-box
+        -webkit-tap-highlight-color: transparent
+
+    html
+        font-family: Open Sans sans-serif
+
+    html[data-theme = "light"]
+        --background-color: bisque
+        --color: black
+
+    html[data-theme = "dark"]
+        --background-color: #282828
+        --color: white
+
+    body
+        margin: 0
+
+    #app
+        width: 100%
+        height: 100vh
+        -webkit-font-smoothing: antialiased
+        -moz-osx-font-smoothing: grayscale
+        color: white
+        background-color: var(--background-color)
+        transition: background-color .5s linear
 </style>
---
+++ src/components/ThemeButton.vue
@@ -0,0 +1,61 @@
+<template lang="pug">
+    button(@click="setTheme")
+</template>
+
+<script>
+    export default {
+        name: "ThemeButton",
+        data() {
+            return {
+                theme: this.getThemeCookie() || "light"
+            }
+        },
+        methods: {
+            changeHTML() {
+                document.querySelector("html").dataset.theme = this.theme
+            },
+            setCookie() {
+                document.cookie = `theme=${ this.theme }`
+            },
+            setTheme() {
+                this.theme = this.theme === "light" ? "dark" : "light"
+            },
+            getThemeCookie: () => document.cookie
+                                            .split(";")
+                                            .map(c => c.trim())
+                                            .filter(c => c.split("=")[0] === "theme")
+                                            .join()
+                                            .split("=")[1]
+        },
+        mounted() {
+            this.changeHTML()
+        },
+        watch: {
+            theme() {
+                this.changeHTML()
+                this.setCookie()
+            }
+        }
+    }
+</script>
+
+<style lang="sass" scoped>
+
+    button
+        position: absolute
+        right: 30px
+        top: 10px
+        width: 60px
+        height: 60px
+        padding: 0
+        border: none
+        cursor: pointer
+        background: url("../assets/themeButton.svg") no-repeat
+        background-size: cover
+        transition: transform .5s ease
+
+    html[data-theme = "light"] button
+        transform: rotate(180deg)
+
+
+</style>
