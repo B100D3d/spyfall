b3835e9e62defdc38bc728eb811cd64c471afcae
devourer <alexblooded@gmail.com>
2020-07-02 16:24:07 +0300

    remove build folder from gitignore

--- build/utils/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.range = void 0;
-exports.range = (size, start = 0) => {
-    return [...Array(size).keys()].map(k => k + start);
-};
